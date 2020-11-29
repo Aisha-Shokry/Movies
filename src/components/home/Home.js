4a5d63ddf50dcfb5d244c42113f2a0b9704d03ba
mona ahmed <monaahmed1994@gmail.com>
2020-11-29 14:46:32 +0200

    new build

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,10 +5,13 @@ import SpinnerLoad from "./SpinnerLoad";
 import MoviesContainer from "./MoviesContainer";
 class Home extends Component {
   render() {
+    console.log(this.props.loading);
     return (
       <div className="HomePage">
         <SearchForm />
-        {this.props.loading ? <SpinnerLoad /> : <MoviesContainer />}
+        {/* {this.props.loading ? <SpinnerLoad /> :  */}
+        <MoviesContainer />
+        {/* } */}
       </div>
     );
   }
